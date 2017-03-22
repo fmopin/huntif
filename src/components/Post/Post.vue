fee2a5048816db23a1a7b8c905e37f5e7a999075
Florian Mopin <contact@florianmopin.com>
2017-03-22 11:46:28 +0100

    Import Charjs et initiate a graph

--- src/components/Post/Post.vue
+++ src/components/Post/Post.vue
@@ -12,6 +12,8 @@
   <span> Post Name: </span>
   <span>{{this.$route.params.postName}}</span>
 
+  <canvas id="myChart" width="400" height="400"></canvas>
+
 </div>
 </template>
 
@@ -21,6 +23,7 @@
   // Import de certaines librairies nécessaires dans ce composant
   import axios from 'axios'
   import _ from 'lodash'
+  import Chart from 'chart.js'
 
   export default {
 
@@ -86,8 +89,50 @@
 
         hours.forEach(function(i) { count[i] = (count[i]||0)+1;  });
         this.dataSet = count
+        this.createChart()
+      },
+
+      createChart() {
+
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        })
       }
-    }
   }
+}
 
 </script>
